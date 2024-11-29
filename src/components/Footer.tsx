fbe9d767682d97dc72f6352fd1b36ed99f808e4b
srinisLearning <srinislearning@gmail.com>
2024-11-29 14:32:27 +0530

    General Changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
 const Footer = () => {
   return (
     <>
-      <div className="flex min-h-16 p-6 bg-green-aaa text-white justify-around items-center gap-4 rounded-t-3xl m-2">
+      <div className="flex min-h-16 p-6 bg-green-900 text-white justify-around items-center gap-4 rounded-t-3xl m-2">
         <div className="flex">
           <div className="flex gap-6 justify-center items-center">
             <Image
@@ -30,22 +30,25 @@ const Footer = () => {
             />
           </div>
         </div>
-        <div className="flex gap-4">
+        <div className="flex flex-col justify-center align-middle gap-4">
+          <div className="flex gap-6">
           <FontAwesomeIcon icon={faFacebookF} size="xl" />
           <FontAwesomeIcon icon={faYoutube} size="xl" />
           <FontAwesomeIcon icon={faTwitter} size="xl" />
           <FontAwesomeIcon icon={faGooglePay} size="xl" />
           <FontAwesomeIcon icon={faLinkedin} size="xl" />
           <FontAwesomeIcon icon={faFacebookF} size="lg" />
-        </div>
-        <div className="flex">
+          </div>
+          <div>
           <Image
             src="/images/comodo_secure.png"
             alt="Good Lifes"
             width={125}
             height={75}
           />
+          </div>
         </div>
+
         <div className="flex">
           <ul className="flex flex-col gap-1">
             <li className="text-md uppercase  text-black-500">About Us</li>
