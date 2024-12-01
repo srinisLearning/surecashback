7122ef722273f0d3366c576b87000c1349cd27b6
srinisLearning <srinislearning@gmail.com>
2024-12-01 08:17:17 +0530

    General Message

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <body>
           <ClerkLoading>
             <div className="flex flex-col justify-center items-center h-screen text-2xl">
